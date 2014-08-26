c8c5c67d7c1410bd98f7908f166850b7b21922d4
Angel Solorio <angel.solorio@me.com>
2014-08-26 18:02:40 -0500

    Refactors code to avoid to reset the view controllers every time they are selected from the main menu

--- Booking Room/ViewControllers/LeftMenuViewController.m
+++ Booking Room/ViewControllers/LeftMenuViewController.m
@@ -54,6 +54,12 @@
     accountStore = [[ACAccountStore alloc] init];
 
     [self userLogged];
+
+    // Loads the View Controllers
+    navHomeController = [[UINavigationController alloc] initWithRootViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"homeController"]];
+    navMyBookingsController = [[UINavigationController alloc] initWithRootViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"myBookingsController"]];
+    navRoomsController = [[UINavigationController alloc] initWithRootViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"meetingRoomsController"]];
+    navCommentsController = [[UINavigationController alloc] initWithRootViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"suggestionsController"]];
 }
 
 
@@ -121,23 +127,19 @@
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     switch (indexPath.row) {
         case 0: // HOME
-            [self.sideMenuViewController setContentViewController:[[UINavigationController alloc] initWithRootViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"homeController"]]
-                                                         animated:YES];
+            [self.sideMenuViewController setContentViewController:navHomeController animated:YES];
             [self.sideMenuViewController hideMenuViewController];
             break;
         case 1: // MY BOOKINGS
-            [self.sideMenuViewController setContentViewController:[[UINavigationController alloc] initWithRootViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"myBookingsController"]]
-                                                         animated:YES];
+            [self.sideMenuViewController setContentViewController:navMyBookingsController animated:YES];
             [self.sideMenuViewController hideMenuViewController];
             break;
         case 2: // MEETING ROOMS
-            [self.sideMenuViewController setContentViewController:[[UINavigationController alloc] initWithRootViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"bookingRoomsController"]]
-                                                         animated:YES];
+            [self.sideMenuViewController setContentViewController:navRoomsController animated:YES];
             [self.sideMenuViewController hideMenuViewController];
             break;
         case 3: // COMMENTS
-            [self.sideMenuViewController setContentViewController:[[UINavigationController alloc] initWithRootViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"suggestionsController"]]
-                                                         animated:YES];
+            [self.sideMenuViewController setContentViewController:navCommentsController animated:YES];
             [self.sideMenuViewController hideMenuViewController];
             break;
         case 4: // RESET PIN
